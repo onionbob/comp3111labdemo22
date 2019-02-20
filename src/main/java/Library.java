213395859e4651cee00d599facc3be18ab0d6246
Admin <Admin@10.89.133.149>
2019-02-20 15:12:49 +0800

    Second Setup

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,6 +4,8 @@
 public class Library {
 	
 	public static void main(String args[]) {
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
 		System.out.println("When there is a fire, commit and push.");
 	}
 	
